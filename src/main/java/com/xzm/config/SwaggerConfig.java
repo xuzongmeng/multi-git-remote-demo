b1f788449016ce1308643014ae5aeba873d78877
xuzongmeng <1102207843@qq.com>
2020-08-24 21:23:45 +0800

    增加本地和远程接口

--- src/main/java/com/xzm/config/SwaggerConfig.java
+++ src/main/java/com/xzm/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * http://localhost:8000/swagger-ui.html
- * http://localhost:8000/doc.html
+ * http://localhost:8000/doc.html1
  */
 
 @Configuration
